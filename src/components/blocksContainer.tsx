1b91f39b7c87bc0468cd67a28d524ea041476e75
Thi <thi@ideta.io>
2023-04-01 18:06:48 +0200

    Basic BlocksContainer

--- src/components/blocksContainer.tsx
+++ src/components/blocksContainer.tsx
@@ -1,14 +1,43 @@
 import cn from 'classnames'
 
+import { fakeNotes } from '../lib/fakeData'
+
 type BlocksContainerProps = {
-  children: React.ReactNode
   className?: string
 }
 
 export default function BlocksContainer(props: BlocksContainerProps) {
   return (
-    <div className={cn('flex min-h-screen flex-col justify-between bg-gray-50', props.className)}>
-      {props.children}
+    <div className={cn('pt-14 h-full flex bg-gray-50', props.className)}>
+      <div className={cn('h-full w-full overflow-auto an-scrollbar')}>
+        <div className={cn('container mx-auto flex flex-col w-full items-center p-6 xl:max-w-4xl')}>
+          <div className={cn('flex gap-4 flex-wrap justify-center')}>
+            {fakeNotes().map(note => (
+              <div
+                key={note.id}
+                className={cn(
+                  'flex flex-1 flex-col gap-2 justify-center items-center',
+                  'min-w-[300px] max-w-[500px] min-h-[200px]',
+                  'bg-white an-shadow-soft-xl rounded-2xl an-bg-clip-border'
+                )}
+              >
+                <div className={cn('flex flex-col gap-2 justify-center items-center')}>
+                  <div className={cn('flex gap-2 justify-center items-center')}>
+                    <div className={cn('w-4 h-4 rounded-full bg-gray-300')} />
+                    <div className={cn('w-4 h-4 rounded-full bg-gray-300')} />
+                    <div className={cn('w-4 h-4 rounded-full bg-gray-300')} />
+                  </div>
+                  <div className={cn('w-12 h-12 rounded-full bg-gray-300')} />
+                </div>
+                <div className={cn('flex flex-col gap-2 justify-center items-center')}>
+                  <div className={cn('w-24 h-4 rounded-full bg-gray-300')} />
+                  <div className={cn('w-24 h-4 rounded-full bg-gray-300')} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
